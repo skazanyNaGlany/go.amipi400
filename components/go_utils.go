a092d2e810e32611fb7bdfe0cd9c7a31b5d05084
skazanyNaGlany <screamingbox@gmail.com>
2022-12-17 12:49:23 +0100

    Implemented very useful GoUtils.GetScriptOrExecutable

--- components/go_utils.go
+++ components/go_utils.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/shirou/gopsutil/v3/process"
 )
 
 type GoUtils struct{}
@@ -40,4 +43,67 @@ func (gu *GoUtils) DuplicateLog() string {
 	log.SetOutput(mw)
 
 	return logFilename
+}
+
+func (gu *GoUtils) GetProcess(pid int32) (*process.Process, error) {
+	processes, err := process.Processes()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, process := range processes {
+		if process.Pid == pid {
+			return process, nil
+		}
+	}
+
+	return nil, nil
+}
+
+// If you run your app by executing build binary, it will return the path to your binary.
+// If you run your app by executing a .go file with go run file.go command, it will try to
+// return path to the script.
+//
+// Only plain "go run script.go" syntax is supported.
+//
+// Requires github.com/shirou/gopsutil/v3/process
+func (gu *GoUtils) GetScriptOrExecutable() (string, error) {
+	process, err := gu.GetProcess(int32(os.Getppid()))
+
+	if err != nil || process == nil {
+		return os.Executable()
+	}
+
+	cmdLine, err := process.Cmdline()
+
+	if err != nil {
+		return "", err
+	}
+
+	before, after, found := strings.Cut(cmdLine, " run ")
+
+	if !found {
+		return os.Executable()
+	}
+
+	before = strings.TrimSpace(before)
+
+	if before != "go" && before != "go.exe" {
+		return os.Executable()
+	}
+
+	after = strings.TrimSpace(after)
+
+	if !strings.HasSuffix(after, ".go") {
+		return os.Executable()
+	}
+
+	pcwd, err := process.Cwd()
+
+	if err != nil {
+		return os.Executable()
+	}
+
+	return filepath.Join(pcwd, after), nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,16 @@ module github.com/skazanyNaGlany/go.amipi400
 go 1.19
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/shirou/gopsutil/v3 v3.22.11 // indirect
 	github.com/thoas/go-funk v0.9.2 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/winfsp/cgofuse v1.5.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 )
