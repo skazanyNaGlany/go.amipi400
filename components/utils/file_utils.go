8e2528baf465beb634741f93a2ffc9764520d894
skazanyNaGlany <screamingbox@gmail.com>
2023-01-04 02:18:54 +0100

    FileUtils.GetDirSize

--- components/utils/file_utils.go
+++ components/utils/file_utils.go
@@ -12,6 +12,24 @@ type FileUtils struct{}
 
 var FileUtilsInstance FileUtils
 
+func (fu *FileUtils) GetDirSize(path string) (int64, error) {
+	var size int64
+
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			size += info.Size()
+		}
+
+		return err
+	})
+
+	return size, err
+}
+
 func (fu *FileUtils) GetDirFiles(dir string) []string {
 	result := make([]string, 0)
 	files, _ := ioutil.ReadDir(dir)
